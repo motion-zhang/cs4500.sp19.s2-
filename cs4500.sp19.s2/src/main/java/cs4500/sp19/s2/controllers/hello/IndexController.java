1b01b908aebd654dc3dd880f20cc3e20fe9ea1bb
wolf-zhang <zhang.li@husky.neu.edu>
2019-01-23 21:13:49 -0500

    first commit

---
+++ cs4500.sp19.s2/src/main/java/cs4500/sp19/s2/controllers/hello/IndexController.java
@@ -0,0 +1,20 @@
+package cs4500.sp19.s2.controllers.hello;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+}

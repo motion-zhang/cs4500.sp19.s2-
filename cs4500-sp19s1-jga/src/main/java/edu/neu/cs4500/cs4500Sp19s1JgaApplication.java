bac7e5def6c63bde497aff8c4ec58b5f2a16c8ae
wolf-zhang <zhang.li@husky.neu.edu>
2019-01-23 20:02:12 -0500

    first commit

---
+++ cs4500-sp19s1-jga/src/main/java/edu/neu/cs4500/cs4500Sp19s1JgaApplication.java
@@ -0,0 +1,14 @@
+package edu.neu.cs4500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cs4500Sp19s1JgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(cs4500Sp19s1JgaApplication.class, args);
+	}
+
+}
+
